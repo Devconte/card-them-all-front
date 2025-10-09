e5a079ad062124d4000127789b5e35c0608e4bd0
Devconte <romain.lcnte@gmail.com>
2025-10-09 21:39:40 +0200

    fix: remove pink striped background from homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -120,23 +120,6 @@ onMounted(() => {
   position: relative;
 }
 
-.main-content::before {
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: 100%;
-  background: repeating-linear-gradient(
-    90deg,
-    transparent,
-    transparent 20px,
-    rgba(255, 192, 203, 0.1) 20px,
-    rgba(255, 192, 203, 0.1) 21px
-  );
-  pointer-events: none;
-}
-
 .section-title {
   text-align: center;
   color: #2b499b;
